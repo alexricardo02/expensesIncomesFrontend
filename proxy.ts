ec352f223446eca507e6953ad2aadb88930017bc
alexricardo02 <brinckmannalex@gmail.com>
2026-04-17 00:29:00 +0200

    now its all fixed

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/dev/server';
-import type { NextRequest } from 'next/dev/server';
+import { NextResponse } from 'next/server';;
+import type { NextRequest } from 'next/server';;
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // 1. El guardia busca tu cookie
   const token = request.cookies.get('auth_token')?.value;
 
